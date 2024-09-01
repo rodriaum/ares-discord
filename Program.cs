34cce24002c88c7e4689dc64989fb38adc3b78a7
Rodrigo Ferreira <rodrigofxxx04@gmail.com>
2024-09-01 07:11:42 +0100

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,83 @@
+﻿using Discord.WebSocket;
+using Discord;
+using Discord.Net;
+using Newtonsoft.Json;
+using Discord_OpenAI.commands;
+
+namespace Discord_OpenAI
+{
+    internal class Program
+    {
+
+        public static DiscordSocketClient? Client { get; private set; }
+
+        static async Task Main()
+        {
+            string token = "MTI0OTE0MzQwMjAwODk0MDYwNQ.GD7gzM.odgXvJMIxT2EcljWyI-Wg5LDRf-7hA4hgKt2SI";
+
+            var config = new DiscordSocketConfig()
+            {
+                GatewayIntents = GatewayIntents.All
+            };
+
+            Client = new DiscordSocketClient(config);
+
+            await Client.LoginAsync(TokenType.Bot, token);
+
+            Client.MessageUpdated += MessageUpdated;
+            await Client.StartAsync();
+
+            new PingCommand(Client);
+
+            Client.Ready += RegisterCommands;
+            Client.Ready += () =>
+            {
+                SocketSelfUser selfUser = Client.CurrentUser;
+
+                Console.WriteLine($"Sucess! Logged to {selfUser.Username}#{selfUser.Discriminator}");
+                return Task.CompletedTask;
+            };
+
+            await Task.Delay(Timeout.Infinite);
+        }
+
+        public static async Task RegisterCommands()
+        {
+            List<SlashCommandBuilder> commands = new List<SlashCommandBuilder>();
+
+            commands.Add(new SlashCommandBuilder()
+                .WithName("ping")
+                .WithDescription("Comando gostoso"));
+
+            try
+            {
+                // Função executada apenas após a conexão bem-sucedida. Mas evita alertas.
+                if (Client == null)
+                    return;
+
+                foreach (SlashCommandBuilder command in commands)
+                {
+                    var build = command.Build();
+                    
+                    await Client.CreateGlobalApplicationCommandAsync(build);
+
+                    Console.WriteLine($"Command {build.Name} registered successfully.");
+                }
+
+            }
+            catch (HttpException e)
+            {
+                string json = JsonConvert.SerializeObject(e.Errors, Formatting.Indented);
+
+                Console.WriteLine("Unable to register commands.\n -> " + (!(string.IsNullOrEmpty(json) || json.Equals("[]")) ? json : e.Message));
+            }
+        }
+
+        private static async Task MessageUpdated(Cacheable<IMessage, ulong> before, SocketMessage after, ISocketMessageChannel channel)
+        {
+            // If the message was not in the cache, downloading it will result in getting a copy of `after`.
+            var message = await before.GetOrDownloadAsync();
+            Console.WriteLine($"{message} -> {after}");
+        }
+    }
+}
---
+++ commands/PingCommand.cs
@@ -0,0 +1,19 @@
+﻿using Discord.WebSocket;
+
+namespace Discord_OpenAI.commands
+{
+    internal class PingCommand
+    {
+        public PingCommand(DiscordSocketClient client)
+        {
+            client.SlashCommandExecuted += SlashCommandHandler;
+        }
+
+        private async Task SlashCommandHandler(SocketSlashCommand command)
+        {
+            if (!command.Data.Name.Equals("ping")) return;
+
+            await command.RespondAsync($"You executed {command.Data.Name}");
+        }
+    }
+}
