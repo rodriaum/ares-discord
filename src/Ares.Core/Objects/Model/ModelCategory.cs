5ad81d95a2491a0e096b6c9e3f36f326c14e3d61
Rodriaum <rodrigofxxx04@gmail.com>
2025-03-28 23:59:13 +0000

    Model web api infos

--- src/Ares.Core/Objects/Model/ModelCategory.cs
+++ src/Ares.Core/Objects/Model/ModelCategory.cs
@@ -1,8 +1,37 @@
-﻿namespace Ares.Core.Objects.Model;
+﻿using System.Reflection;
+
+namespace Ares.Core.Objects.Model;
 
 public enum ModelCategory
 {
+    [WebApi("https://api.openai.com/v1")]
     OpenAI,
+
+    [WebApi("https://api.anthropic.com/v1/")]
     Anthropic,
+
+    [WebApi("https://api.deepseek.com")]
     DeepSeek
+}
+
+[AttributeUsage(AttributeTargets.Field)]
+public class WebApiAttribute : Attribute
+{
+    public string Endpoint { get; }
+    public WebApiAttribute(string url) => Endpoint = url;
+}
+
+public static class ModelCategoryExtensions
+{
+    public static Uri? GetEndpoint(this ModelCategory category)
+    {
+        Type type = category.GetType();
+        MemberInfo[] member = type.GetMember(category.ToString());
+        WebApiAttribute? attribute = member[0].GetCustomAttribute<WebApiAttribute>();
+
+        if (attribute == null)
+            return null;
+
+        return new Uri(attribute.Endpoint) ?? null;
+    }
 }
