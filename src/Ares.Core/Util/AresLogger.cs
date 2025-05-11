82a25b065da95df197e8dd484c946e4401ebf804
Rodriaum <rodrigofxxx04@gmail.com>
2025-05-11 22:14:48 +0100

    Color in log

--- src/Ares.Core/Util/AresLogger.cs
+++ src/Ares.Core/Util/AresLogger.cs
@@ -5,28 +5,46 @@
  */
 
 using Ares.Core.Objects;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Diagnostics;
 
 namespace Ares.Core.Util;
 
 public class AresLogger
 {
-    /* Variables */
-
     private static DateTime time = DateTime.UtcNow;
 
-    /* Methods */
+    /* Functions */
 
     public static void Log(string prefix, string message, string extra = "", Severity severity = Severity.Info)
     {
+        Console.ForegroundColor = GetColorBySeverity(severity);
         Console.Error.WriteLine(Output(prefix, message, extra: extra, severity: severity));
+        Console.ResetColor();
     }
 
     public static async Task LogAsync(string prefix, string message, string extra = "", Severity severity = Severity.Info)
     {
+        Console.ForegroundColor = GetColorBySeverity(severity);
         await Console.Error.WriteLineAsync(Output(prefix, message, extra: extra, severity: severity));
+        Console.ResetColor();
     }
 
     /* Helpers */
+
+    private static ConsoleColor GetColorBySeverity(Severity severity)
+    {
+        return severity switch
+        {
+            Severity.Critical => ConsoleColor.DarkRed,
+            Severity.Error => ConsoleColor.Red,
+            Severity.Warning => ConsoleColor.Yellow,
+            Severity.Info => ConsoleColor.Gray,
+            Severity.Verbose => ConsoleColor.White,
+            Severity.Debug => ConsoleColor.DarkGray,
+            _ => ConsoleColor.Gray
+        };
+    }
 
     private static string Output(string prefix, string message, string extra = "", Severity severity = Severity.Info)
     {
